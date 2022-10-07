02316f31f3ef9218307d7d6cbd7c71b37180f2b2
adammassias-161803 <115151881+adammassias-161803@users.noreply.github.com>
2022-10-07 09:34:05 +0100

    Create AWS_methods.py

---
+++ constructors/AWS_methods.py
@@ -0,0 +1,34 @@
+import base64
+import pandas as pd
+import boto3
+from io import StringIO
+import json
+
+class S3_execution_class:
+    
+    def pull_S3_files(self, bucket_name, file_name):
+
+        s3 = boto3.client('s3')
+        obj = s3.get_object(Bucket = bucket_name, Key = file_name)
+        df = obj['Body']
+        df_pull_decode  = df.read().decode('utf-8')
+
+        return pd.read_csv(StringIO(df_pull_decode))
+
+    def push_S3_files(self, bucket_name, file_name, dataframe):
+
+        csv_buffer = StringIO()
+        dataframe.to_csv(csv_buffer, index = False)
+        s3_resource = boto3.resource('s3')
+        s3_resource.Object(bucket_name, file_name).put(Body=csv_buffer.getvalue())
+    
+    def get_sm_secret(self, secret_name, region_name):
+        
+        client = boto3.client('secretsmanager', region_name=region_name )
+        response = client.get_secret_value(
+            SecretId=secret_name
+        )
+
+        sm_secrets = json.loads(response['SecretString'])
+
+        return sm_secrets
